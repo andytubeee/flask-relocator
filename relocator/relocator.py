e173e79878ff9bf0a7b66b83d927ca25675ace19
Andrew Yang <ay3yang@gmail.com>
2024-06-26 15:02:29 -0400

    added redirect logic, and set it as a tunable param

--- relocator/relocator.py
+++ relocator/relocator.py
@@ -10,6 +10,10 @@ relocator_blueprint = Blueprint('relocator', __name__, template_folder='template
 
 # Read environment variables
 new_location = os.getenv('NEW_LOCATION')
+redirect = os.getenv('REDIRECT', 'True') in ('True', 'true', '1', 't', 'T', 'Ya', 'ya', 'Yes', 'yes')
+
+if new_location and not new_location.startswith(('http://', 'https://')):
+    new_location = 'http://' + new_location
 
 @relocator_blueprint.route('/', defaults={'path': ''})
 @relocator_blueprint.route('/<path:path>')
@@ -18,4 +22,4 @@ def index(path):
         message = f"Server is migrated to <a href='{new_location}'>{new_location}</a>"
     else:
         message = "The server is migrated somewhere else and is no longer here..."
-    return render_template('index.html', message=message)
+    return render_template('index.html', message=message, redirect=redirect, new_location=new_location)
